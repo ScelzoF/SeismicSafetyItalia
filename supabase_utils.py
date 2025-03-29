e9dcf7254915df2a0966bd87390524f0aac05f0f
ScelzoF <112801219+ScelzoF@users.noreply.github.com>
2025-03-29 17:25:01 +0100

    Add files via upload

--- supabase_utils.py
+++ supabase_utils.py
@@ -4,7 +4,7 @@ import requests
 SUPABASE_URL = "https://ljrjaehrttxhqejcueqj.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqcmphZWhydHR4aHFlamN1ZXFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxODU4OTcsImV4cCI6MjA1ODc2MTg5N30.wfbzum88wn0b0OVw6WlMunOWvLvnfIjqnRyGeEQLghY"
 
-SUPABASE_HEADERS = {
+SUPABASE_SUPABASE_HEADERS = {
     "apikey": SUPABASE_KEY,
     "Authorization": f"Bearer {SUPABASE_KEY}",
     "Content-Type": "application/json"
@@ -17,7 +17,7 @@ def inserisci_post(username, contenuto):
     }
     response = requests.post(
         f"{SUPABASE_URL}/rest/v1/chat_altro_progetto",
-        headers=SUPABASE_HEADERS,
+        headers=SUPABASE_SUPABASE_HEADERS,
         json=data
     )
     if response.status_code != 201:
@@ -27,7 +27,7 @@ def inserisci_post(username, contenuto):
 def carica_post():
     response = requests.get(
         f"{SUPABASE_URL}/rest/v1/chat_altro_progetto?select=*",
-        headers=SUPABASE_HEADERS
+        headers=SUPABASE_SUPABASE_HEADERS
     )
     if response.status_code == 200:
         return response.json()
@@ -42,7 +42,7 @@ def invia_segnalazione(localita, tipo_evento, intensita, descrizione):
     }
     response = requests.post(
         f"{SUPABASE_URL}/rest/v1/segnalazioni_altro_progetto",
-        headers=SUPABASE_HEADERS,
+        headers=SUPABASE_SUPABASE_HEADERS,
         json=data
     )
     if response.status_code != 201:
@@ -57,7 +57,7 @@ def inserisci_segnalazione(username, contenuto):
             "username": username or "Anonimo",
             "contenuto": contenuto,
         }
-        res = requests.post(f"{SUPABASE_URL}/rest/v1/segnalazioni_altro_progetto", json=data, headers=HEADERS)
+        res = requests.post(f"{SUPABASE_URL}/rest/v1/segnalazioni_altro_progetto", json=data, headers=SUPABASE_HEADERS)
         res.raise_for_status()
         return True, "✅ Segnalazione inviata con successo."
     except Exception as e:
@@ -65,7 +65,7 @@ def inserisci_segnalazione(username, contenuto):
 
 def carica_segnalazioni():
     try:
-        res = requests.get(f"{SUPABASE_URL}/rest/v1/segnalazioni_altro_progetto?select=*", headers=HEADERS)
+        res = requests.get(f"{SUPABASE_URL}/rest/v1/segnalazioni_altro_progetto?select=*", headers=SUPABASE_HEADERS)
         res.raise_for_status()
         return res.json()
     except Exception as e:
