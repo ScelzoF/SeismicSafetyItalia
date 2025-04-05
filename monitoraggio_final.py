89bea1823c9f6fcd65001187b8fd7482cddac10a
ScelzoF <112801219+ScelzoF@users.noreply.github.com>
2025-04-05 12:38:30 +0200

    Add files via upload

--- monitoraggio_final.py
+++ monitoraggio_final.py
@@ -4,11 +4,16 @@ from fallback_sismico import get_sismic_data
 
 def process_data():
     st.subheader("Dati sismici in tempo reale")
+    debug = st.sidebar.checkbox("Mostra dettagli tecnici")
+
     with st.spinner("🔄 Recupero dati sismici..."):
         df, fonte = get_sismic_data()
 
+    if debug:
+        st.success("✅ DEBUG: process_data è stata eseguita!")
+
     if not df.empty:
         st.info(fonte)
-        st.dataframe(df)
+        st.dataframe(df, use_container_width=True)
     else:
         st.warning(fonte)
