73970fa012078eb6db375deca9b26d1b9674f2c1
ScelzoF <112801219+ScelzoF@users.noreply.github.com>
2025-07-02 09:12:06 +0200

    Add files via upload

--- visualization.py
+++ visualization.py
@@ -680,7 +680,7 @@ go.Indicator(
         xaxis_showgrid=True
     )
 
-    st.plotly_chart(fig, use_container_width=True, key="magnitude_time_chart")
+    st.plotly_chart(fig, use_container_width=True)
 
     # Calculate recent and older counts
     recent_dates = [k for k in stats['daily_counts'].keys() 
